856da1f87c5f445d3c9bb19ba13dd6acaed3204f
Aline Neves <alineves@gmail.com>
2016-12-19 21:48:43 -0200

    init

--- src/exercicio1/Exercicio1.java
+++ src/exercicio1/Exercicio1.java
@@ -4,4 +4,7 @@ package exercicio1;
  * Created by Aline on 19/12/2016.
  */
 public class Exercicio1 {
+    public static void main (String[] args){
+        System.out.println("Meu amor deu certo!");
+    }
 }
